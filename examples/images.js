caadbc61d782dc2aed654b49aa37e061ba864159
Gaetano Perrone <gaetanoperrone@MBP-di-Gaetano.fritz.box>
2017-05-04 18:36:55 +0200

    Update

--- examples/images.js
+++ examples/images.js
@@ -3,11 +3,11 @@ const dockerImages = require('../lib/image-mgr.js'),
 
 
 //Get image names  
-dockerImages.getNames(function(err, json) {
-	utils.print(err,json)
-}, {onlytagged:true})
-
-
+//dockerImages.getNames(function(err, json) {
+//	utils.print(err,json)
+//}, {onlytagged:true})
+//
+//
 //Get detailed list of images in javascript object
 dockerImages.getDetailedList(function(err, data) {
 	utils.print(err,data)
