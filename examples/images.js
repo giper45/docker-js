2ebd597aaf1c4edf3d9f9afb2ab454f5d3828408
Gaetano Perrone <>
2018-06-17 22:58:10 +0200

    Fix tag imag list

--- examples/images.js
+++ examples/images.js
@@ -3,35 +3,35 @@ const utils = require('./utils.js');
 const _ = require('underscore')
 const util = require('util')
 
-String.prototype.paddingRight = function (paddingValue) {
-   return String(this + paddingValue).slice(-paddingValue.length);
-};
+// String.prototype.paddingRight = function (paddingValue) {
+//    return String(this + paddingValue).slice(-paddingValue.length);
+// };
 
-let ids = []
-function formatPull(json) {
-  splitted = json.split(/\r\n|\r|\n/)
-  for (i = 0; i < splitted.length-1; i++) {
-    s = splitted[i];
-    ss = JSON.parse(s)
-    if (!ss.progress)
-      ss.progress=""
-    const status = ss.status
-    const progress = ss.progress
-    const id = ss.id
+// let ids = []
+// function formatPull(json) {
+//   splitted = json.split(/\r\n|\r|\n/)
+//   for (i = 0; i < splitted.length-1; i++) {
+//     s = splitted[i];
+//     ss = JSON.parse(s)
+//     if (!ss.progress)
+//       ss.progress=""
+//     const status = ss.status
+//     const progress = ss.progress
+//     const id = ss.id
 
 
-  }
+//   }
 
-}
-dockerImages.removeImage("vimagick/webgoat", "latest", (err, data) => {
-  if (err) {
-    console.log("ERROR");
-    console.log(err);
-  }
-  else {
-    console.log(data);
-  }
-})
+// }
+// dockerImages.removeImage("vimagick/webgoat", "latest", (err, data) => {
+//   if (err) {
+//     console.log("ERROR");
+//     console.log(err);
+//   }
+//   else {
+//     console.log(data);
+//   }
+// })
 // dockerImages.pullImage("ubuntu", "latest", (err, data) => {
 //   // console.log("STATUS".padEnd(spanSpaces), "PROGRESS".padEnd(spanSpaces), "ID".padEnd(spanSpaces))
 //   if (err) {
@@ -48,9 +48,9 @@ dockerImages.removeImage("vimagick/webgoat", "latest", (err, data) => {
 
 // Get detailed list of images in javascript object
 
-// dockerImages.getDetailedList((err, data) => {
-//   utils.print(err, data);
-// });
+dockerImages.getDetailedList((err, data) => {
+   utils.print(err, data);
+});
 
 
 // // Get json list of images
